3332b58bcd919f85671e41186387a6a79a97e691
woongblack <woongblack123@gmail.com>
2025-12-06 02:02:02 +0900

    feat : lombok 제거 및 getter 생성

--- src/main/java/com/example/shortudy/global/common/BaseTimeEntity.java
+++ src/main/java/com/example/shortudy/global/common/BaseTimeEntity.java
@@ -3,22 +3,20 @@ package com.example.shortudy.global.common;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
-@Getter // 1. Lombok으로 간결하게 Getter 생성
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Comment("생성 일시") //
+    @Comment("생성 일시")
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
@@ -26,4 +24,12 @@ public abstract class BaseTimeEntity {
     @Comment("수정 일시")
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
