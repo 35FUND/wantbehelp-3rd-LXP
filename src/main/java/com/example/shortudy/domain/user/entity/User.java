9c7891591dd4da5afe29f9dcde57db70e019c2d0
woongblack <woongblack123@gmail.com>
2025-12-06 02:07:03 +0900

    feat : create mock User Entity,repo

--- src/main/java/com/example/shortudy/domain/user/entity/User.java
+++ src/main/java/com/example/shortudy/domain/user/entity/User.java
@@ -1,5 +1,33 @@
 package com.example.shortudy.domain.user.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//쉐도우 복싱용 유저
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+
+    public User(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
 
--- src/main/java/com/example/shortudy/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/shortudy/domain/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.shortudy.domain.user.repository;
 
+import com.example.shortudy.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//쉐도우 복싱용 유저
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long>
+{
 }
 
