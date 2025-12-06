7d418d4924721c1853194c09a8957360b887168c
woongblack <woongblack123@gmail.com>
2025-12-06 23:32:28 +0900

    feat : 오타 수정 및 쿼리문제 해결

--- src/main/java/com/example/shortudy/domain/shorts/repository/ShortsRepository.java
+++ src/main/java/com/example/shortudy/domain/shorts/repository/ShortsRepository.java
@@ -1,10 +1,17 @@
 package com.example.shortudy.domain.shorts.repository;
 
 import com.example.shortudy.domain.shorts.entity.Shorts;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShortsRepository extends JpaRepository<Shorts, Long> {
+
+    @EntityGraph(attributePaths = {"user", "category"})
+    Optional<Shorts> findWithDetailsById(Long id);
 }
 
