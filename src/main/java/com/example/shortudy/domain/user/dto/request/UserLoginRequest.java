7cc8d9f2ffc2cc3aa6ab09d4341e20f11a90ad21
zico24820-dev <zico24820@gmail.com>
2025-12-09 17:44:04 +0900

    refactor:로그인,로그아웃 API

--- src/main/java/com/example/shortudy/domain/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/example/shortudy/domain/user/dto/request/UserLoginRequest.java
@@ -1,30 +1,22 @@
 package com.example.shortudy.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserLoginRequest {
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
-
-    public UserLoginRequest() {
-    }
-
-    public UserLoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
---
+++ src/main/java/com/example/shortudy/domain/user/dto/response/UserLoginResponse.java
@@ -0,0 +1,30 @@
+package com.example.shortudy.domain.user.dto.response;
+
+public class UserLoginResponse {
+    private String accessToken;
+    private String refreshToken;
+
+    public UserLoginResponse() {
+    }
+
+    public UserLoginResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
