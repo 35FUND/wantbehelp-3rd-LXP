1f909c030b15b3b467491bc68550149d49695dad
JinYong833 <jjystudy1@gmail.com>
2025-12-29 20:05:44 +0900

    refactor[package] : 파일 삭제

--- src/main/java/com/example/shortudy/global/error/exception/InvalidPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.example.shortudy.global.error.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
-
