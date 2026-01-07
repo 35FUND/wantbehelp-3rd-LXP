1b5fe02e05baac206cb3925f974cfbaa19bea5e4
JinYong833 <jjystudy1@gmail.com>
2026-01-07 18:42:28 +0900

    refactor[user] : LoginRequest 수정
    - Request 이름 수정 및 swagger 삭제

--- src/main/java/com/example/shortudy/domain/user/dto/request/LoginRequest.java
+++ src/main/java/com/example/shortudy/domain/user/dto/request/LoginRequest.java
@@ -1,16 +1,12 @@
 package com.example.shortudy.domain.user.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-@Schema(description = "로그인 요청")
-public record UserLoginRequest(
-        @Schema(description = "이메일", example = "user@example.com")
+public record LoginRequest(
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email,
 
-        @Schema(description = "비밀번호", example = "password123")
         @NotBlank(message = "비밀번호를 입력해주세요.")
         String password
 ) {
