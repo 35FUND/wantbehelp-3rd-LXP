08d76f787bbfe1ea86aebc11c554235e76cec5bf
JinYong833 <jjystudy1@gmail.com>
2026-01-07 18:44:05 +0900

    feat[user] : RefreshRequest 생성
    - accessToken이 만료되었을 때 필요한 refresh용 Request 생성

---
+++ src/main/java/com/example/shortudy/domain/user/dto/request/RefreshRequest.java
@@ -0,0 +1,10 @@
+package com.example.shortudy.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshRequest(
+
+        @NotBlank(message = "Refresh Token은 필수입니다.")
+        String refreshToken
+) {
+}
