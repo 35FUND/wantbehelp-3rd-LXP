0f1710fdc541be65fe9fb1ad1e0553a0b7c871a8
JinYong833 <jjystudy1@gmail.com>
2026-01-07 18:50:32 +0900

    feat[user] : UserService 추가
    - 유저 정보를 반환하는 getUserInfo 추가 및 회원 가입 및 유저 삭제 수정

--- src/main/java/com/example/shortudy/domain/user/service/UserService.java
+++ src/main/java/com/example/shortudy/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.shortudy.domain.user.service;
 
 import com.example.shortudy.domain.user.dto.request.SignUpRequest;
+import com.example.shortudy.domain.user.dto.response.InfoResponse;
 import com.example.shortudy.domain.user.entity.User;
+import com.example.shortudy.domain.user.entity.UserRole;
 import com.example.shortudy.domain.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,25 +15,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @Transactional
     public void signup(SignUpRequest request) {
 
         //TODO 정책 확정 필요(email, nickname 중복에 관해)
         if (userRepository.existsByEmail(request.email())) throw new IllegalArgumentException("Email already exists");
         if (userRepository.existsByNickname(request.nickname())) throw new IllegalArgumentException("Nickname already exists");
 
-        userRepository.save(User.create(request.email(), request.password(), request.nickname(),));
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        userRepository.save(User.create(
+                request.email(),
+                encodedPassword,
+                request.nickname(),
+                UserRole.USER,
+                request.profileUrl()
+        ));
+    }
+
+    public InfoResponse getUserInfo(Long userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        //TODO InfoResponse의 from 메서드 작성
+        return new InfoResponse(user.getId(), user.getEmail(), user.getNickname(), user.getProfileUrl());
     }
 
     @Transactional
     public void deleteUser(Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다: " + userId));
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         userRepository.delete(user);
     }
 }
