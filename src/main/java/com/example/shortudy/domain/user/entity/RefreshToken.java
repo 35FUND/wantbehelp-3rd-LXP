07785ce71db20ef728f6c92657d21c5aefb547ef
JinYong833 <jjystudy1@gmail.com>
2026-01-07 18:44:47 +0900

    refactor[user] : RefreshToken Entity 수정
    - Getter 추가 및 패키지 위치 수정

--- src/main/java/com/example/shortudy/domain/user/entity/RefreshToken.java
+++ src/main/java/com/example/shortudy/domain/user/entity/RefreshToken.java
@@ -1,4 +1,4 @@
-package com.example.shortudy.domain.auth.entity;
+package com.example.shortudy.domain.user.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -6,7 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "refreshtokens")
 public class RefreshToken {
