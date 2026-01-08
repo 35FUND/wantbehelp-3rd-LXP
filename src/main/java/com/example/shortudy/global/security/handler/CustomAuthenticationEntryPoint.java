0b049062b4dd4cf305ad8d26ddaff22fe00f0126
JinYong833 <jjystudy1@gmail.com>
2026-01-08 16:02:26 +0900

    refactor[user] : CustomAuthenticationEntryPoint 수정
    - errorCode 세분화를 위한 작업 수행

--- src/main/java/com/example/shortudy/global/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/shortudy/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.example.shortudy.global.security.handler;
 
 import com.example.shortudy.global.common.ApiResponse;
+import com.example.shortudy.global.error.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,13 +20,26 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
-        // HTTP 상태 코드를 401(Unauthorized)로 설정
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//         HTTP 상태 코드를 401(Unauthorized)로 설정
+//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ErrorCode errorCode = (ErrorCode) request.getAttribute("exception");
+
+        if (errorCode == null) {
+//            errorCode = errorCode.valueOf(request.getParameter("error"));
+        }
+//        Http 상태 코드 설정
+//        response.setStatus(errorCode.getHHttpStatus.value());
 
         // 응답 타입을 JSON으로 설정
         response.setContentType("application/json;charset=UTF-8");
 
-        ApiResponse<Void> apiResponse = ApiResponse.error("인증 정보가 유효하지 않습니다.");
+        ApiResponse<Void> apiResponse = ApiResponse.error(
+//                errorCode.getMessage(),
+//                errorCode.getCode
+                //TODO 수정 해야 함..
+                null
+        );
 
         response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
     }
