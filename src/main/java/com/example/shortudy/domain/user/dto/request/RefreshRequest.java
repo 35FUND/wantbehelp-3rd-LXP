bc288f4e386ad694bb5cdbe2881a8c842de6bc52
JinYong833 <jjystudy1@gmail.com>
2026-01-08 16:03:44 +0900

    refactor[user] : RefreshRequest 수정
    - 표기법 통일

--- src/main/java/com/example/shortudy/domain/user/dto/request/RefreshRequest.java
+++ src/main/java/com/example/shortudy/domain/user/dto/request/RefreshRequest.java
@@ -3,7 +3,6 @@ package com.example.shortudy.domain.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record RefreshRequest(
-
         @NotBlank(message = "Refresh Token은 필수입니다.")
         String refreshToken
 ) {
