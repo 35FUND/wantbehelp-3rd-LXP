5a2ab2cd88d9e984bc4ac71592ad563a1ec4f180
JinYong833 <jjystudy1@gmail.com>
2026-01-09 14:44:48 +0900

    refactor[user] : ErrorCode 수정
    - Enum 에러코드에 유저 에러 추가

--- src/main/java/com/example/shortudy/global/error/ErrorCode.java
+++ src/main/java/com/example/shortudy/global/error/ErrorCode.java
@@ -8,12 +8,17 @@ public enum ErrorCode {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_400", "요청 값이 올바르지 않습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_405", "지원하지 않는 HTTP 메서드입니다."),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_401", "요청 권한이 없습니다."),
+
 
     // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "사용자를 찾을 수 없습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "리소스를 찾을 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "USER_401", "유효하지 않은 비밀번호입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "USER_409", "이미 사용 중인 이메일입니다."),
+    LOGIN_REQUIRED(HttpStatus.BAD_REQUEST, "USER_400", "로그인이 필요합니다."),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "USER_400", "유효하지 않은 토큰정보입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "USER_401", "토큰이 만료되었습니다."),
 
     // shorts
     SHORTS_NOT_FOUND(HttpStatus.NOT_FOUND, "SHORTS_404", "해당 숏츠를 찾을 수 없습니다."),
