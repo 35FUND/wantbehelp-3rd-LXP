17df49d1140c4239771cd49f80d455d84c38c32a
JinYong833 <jjystudy1@gmail.com>
2026-01-09 14:46:44 +0900

    refactor[user] : InfoResponse 수정
    - 유저 객체를 받아 Response를 반환하는 로직 추가

--- src/main/java/com/example/shortudy/domain/user/dto/response/InfoResponse.java
+++ src/main/java/com/example/shortudy/domain/user/dto/response/InfoResponse.java
@@ -1,9 +1,19 @@
 package com.example.shortudy.domain.user.dto.response;
 
+import com.example.shortudy.domain.user.entity.User;
+
 public record InfoResponse(
         Long userId,
         String email,
         String nickName,
         String profileUrl
 ) {
+
+    public static InfoResponse from(User user) {
+        return new InfoResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getNickname(),
+                user.getProfileUrl());
+    }
 }
