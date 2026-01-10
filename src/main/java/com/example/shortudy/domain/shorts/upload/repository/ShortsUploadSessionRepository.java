9c98485d48e9470d45cd5a10e8c5333351851dd1
minji0828 <entersuk1998@naver.com>
2026-01-10 21:23:00 +0900

    Create ShortsUploadSessionRepository.java

---
+++ src/main/java/com/example/shortudy/domain/shorts/upload/repository/ShortsUploadSessionRepository.java
@@ -0,0 +1,7 @@
+package com.example.shortudy.domain.shorts.upload.repository;
+
+import com.example.shortudy.domain.shorts.upload.entity.ShortsUploadSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShortsUploadSessionRepository extends JpaRepository<ShortsUploadSession, String> {
+}
