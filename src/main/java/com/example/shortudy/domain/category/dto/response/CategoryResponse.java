831700c471cbe0004b6f764f700e25fb9ec0a45b
zico24820-dev <zico24820@gmail.com>
2026-01-12 18:58:21 +0900

    refactor[category] : record로 변경

--- src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
@@ -1,19 +1,11 @@
 package com.example.shortudy.domain.category.dto.response;
 
 import com.example.shortudy.domain.category.entity.Category;
-import lombok.Getter;
 
-@Getter
-public class CategoryResponse{
-    private final Long id;
-    private final String name;
-
-    public CategoryResponse(Long id, String name){
-        this.id = id;
-        this.name = name;
-    }
-    public static CategoryResponse of(Category category){
+public record CategoryResponse(Long id, String name) {
+    public static CategoryResponse of(Category category) {
         return new CategoryResponse(category.getId(), category.getName());
     }
 }
+
 
