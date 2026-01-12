648f66fe691c182c12725469b379fcd97978a08a
zico24820-dev <zico24820@gmail.com>
2026-01-12 15:29:33 +0900

    refactor[category] : 사용하지 않는 어노테이션 제거

--- src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
@@ -1,15 +1,17 @@
 package com.example.shortudy.domain.category.dto.response;
 
 import com.example.shortudy.domain.category.entity.Category;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class CategoryResponse{
     private final Long id;
     private final String name;
 
+    public CategoryResponse(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
     public static CategoryResponse of(Category category){
         return new CategoryResponse(category.getId(), category.getName());
     }
