ba782ae29bc37e9b6cceb7cf0a74adafacc66ce6
JinYong833 <jjystudy1@gmail.com>
2026-01-12 11:48:25 +0900

    feat[user] : UpdateProfileRequest
    - 유저가 프로필 변경하는 요청을 했을 때 사용되는 dto

---
+++ src/main/java/com/example/shortudy/domain/user/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,13 @@
+package com.example.shortudy.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+
+public record UpdateProfileRequest(
+        // 변경사항이 없을 경우가 있어 Null 체크는 하지 않음
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email,
+
+        String nickName,
+        String userProfileUrl
+) {
+}
