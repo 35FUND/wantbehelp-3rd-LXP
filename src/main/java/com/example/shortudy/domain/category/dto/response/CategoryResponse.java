5af5df64614f836d8225146dee3aef592489bd9c
zico24820-dev <zico24820@gmail.com>
2026-01-12 12:04:08 +0900

    refactor[category] : parents id 삭제 및 및 lombok 추가

--- src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/com/example/shortudy/domain/category/dto/response/CategoryResponse.java
@@ -1,19 +1,19 @@
 package com.example.shortudy.domain.category.dto.response;
 
 import com.example.shortudy.domain.category.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Builder;
 
-public record CategoryResponse(
-        Long id,
-        String name,
-        Long parentId
-) {
+@Getter
+@AllArgsConstructor
+@Builder
+public class CategoryResponse{
+    private final Long id;
+    private final String name;
 
     public static CategoryResponse of(Category category) {
-        return new CategoryResponse(
-                category.getId(),
-                category.getName(),
-                category.getParentId()
-        );
+        return new CategoryResponse(category.getId(), category.getName());
     }
 }
 
