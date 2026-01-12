aabe0f09b597d65dfcc3d84f54fac2e7ab81d4be
zico24820-dev <zico24820@gmail.com>
2026-01-12 19:00:50 +0900

    refactor[category] : CreateDate , LastModifiedDate 어노테이션 추가

--- src/main/java/com/example/shortudy/domain/category/entity/Category.java
+++ src/main/java/com/example/shortudy/domain/category/entity/Category.java
@@ -2,9 +2,12 @@ package com.example.shortudy.domain.category.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "category")
 @Getter
@@ -22,9 +25,11 @@ public class Category {
     @Column(nullable = false)
     private CategoryStatus status;
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
