e353a84b08ef65be5b5c6bfe9319d6265dd4c417
zico24820-dev <zico24820@gmail.com>
2026-01-13 14:58:49 +0900

    refactor[keyword] : keyword-entity  - 키워드 엔티티 수정

--- src/main/java/com/example/shortudy/domain/keyword/entity/Keyword.java
+++ src/main/java/com/example/shortudy/domain/keyword/entity/Keyword.java
@@ -3,24 +3,36 @@ package com.example.shortudy.domain.keyword.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-@Entity
 @Getter
+@Entity
+@Table(name = "keyword")
 public class Keyword {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
-    private String name;
+    // 사용자에게 보여줄 값 ex) JAVA
+    @Column(nullable = false, length = 50)
+    private String displayName;
+
+    // 정규화된 값 ex) java (unique)
+    @Column(nullable = false, length = 50, unique = true)
+    private String normalizedName;
 
     protected Keyword() {}
 
-    private Keyword(String name) {
-        this.name = name;
+    public Keyword(String displayName, String normalizedName) {
+        this.displayName = displayName;
+        this.normalizedName = normalizedName;
     }
 
-    public static Keyword of(String name) {
-        return new Keyword(name);
+    /**
+     * 태그 이름 업데이트
+     * - ID를 유지하면서 이름만 변경
+     */
+    public void updateName(String displayName, String normalizedName) {
+        this.displayName = displayName;
+        this.normalizedName = normalizedName;
     }
 }
