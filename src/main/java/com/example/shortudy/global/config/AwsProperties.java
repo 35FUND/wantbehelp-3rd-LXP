ac35b993abf472b5d58a8a6986f2ed096dfbb49a
minji0828 <entersuk1998@naver.com>
2026-01-13 09:04:05 +0900

    Update AwsProperties.java
    롬복 getter 사용 변경

--- src/main/java/com/example/shortudy/global/config/AwsProperties.java
+++ src/main/java/com/example/shortudy/global/config/AwsProperties.java
@@ -1,34 +1,22 @@
 package com.example.shortudy.global.config;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 @ConfigurationProperties(prefix = "aws")
 public class AwsProperties {
 
     private String region;
     private final S3 s3 = new S3();
-
-    public String getRegion() {
-        return region;
-    }
-
     public void setRegion(String region) {
         this.region = region;
     }
 
-    public S3 getS3() {
-        return s3;
-    }
-
     public static class S3 {
         private String bucket;
-
-        public String getBucket() {
-            return bucket;
-        }
-
         public void setBucket(String bucket) {
             this.bucket = bucket;
         }
