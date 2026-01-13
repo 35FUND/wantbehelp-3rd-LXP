872b66aacfe6c4572cb91c9bdd495bf4e8dfd215
JinYong833 <jjystudy1@gmail.com>
2026-01-13 16:39:15 +0900

    refactor[user] : SecurityConfig 수정
    - 엔드포인트 변경으로 인한 수정

--- src/main/java/com/example/shortudy/global/config/SecurityConfig.java
+++ src/main/java/com/example/shortudy/global/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // .permitAll() -> 누구나 접근할 수 있는 권한 제어
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/users/signup").permitAll()
+                        .requestMatchers("/api/v1/users").permitAll()
                         // GET 요청의 특정 데이터 조회는 누구나 가능하다.
                         .requestMatchers(HttpMethod.GET, "/api/v1/shorts/**", "/api/v1/categories/**").permitAll()
 
