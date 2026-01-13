69ea6fe8e88258877d1552d7c813e97fe4d0fd81
zico24820-dev <zico24820@gmail.com>
2026-01-13 16:19:18 +0900

    refactor[keyword] : keyword-KeywordRepo  - 불필요한 코드 제거 및 오타 수정

--- src/main/java/com/example/shortudy/domain/keyword/repository/KeywordRepository.java
+++ src/main/java/com/example/shortudy/domain/keyword/repository/KeywordRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-    Optional<Keyword> findByNormalizedName(String normalizedName, Limit limit);
+    Optional<Keyword> findByNormalizedName(String normalizedName);
     boolean existsByNormalizedName(String normalizedName);
 
 }
