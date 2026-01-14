d8f08344ccf65441c5a07159671b11f8366b106e
zico24820-dev <zico24820@gmail.com>
2026-01-14 16:15:38 +0900

    refactor[keyword] : keyword-response - normalizedName 추가

--- src/main/java/com/example/shortudy/domain/keyword/dto/response/KeywordResponse.java
+++ src/main/java/com/example/shortudy/domain/keyword/dto/response/KeywordResponse.java
@@ -3,4 +3,4 @@ package com.example.shortudy.domain.keyword.dto.response;
 import jakarta.validation.constraints.NotBlank;
 
 public record KeywordResponse(Long id,
-        @NotBlank String displayName){}
+        @NotBlank String displayName,String normalizedName){}
