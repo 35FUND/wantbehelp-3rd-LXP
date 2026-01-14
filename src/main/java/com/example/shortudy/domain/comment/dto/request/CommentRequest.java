d3ad0dca919245874c8ee8d8f447e0ffa6bbb0ed
hojun-lang <milky9925@gmail.com>
2026-01-14 11:24:57 +0900

    style[comment] : import문 최적화, 줄 정리
     import문 최적화, 줄 정리

--- src/main/java/com/example/shortudy/domain/comment/dto/request/CommentRequest.java
+++ src/main/java/com/example/shortudy/domain/comment/dto/request/CommentRequest.java
@@ -3,8 +3,8 @@ package com.example.shortudy.domain.comment.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CommentRequest (
+public record CommentRequest(
 
         @NotBlank @Size(min = 1, max = 1000) String content
-        ){
+) {
 }
--- src/main/java/com/example/shortudy/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/shortudy/domain/comment/repository/CommentRepository.java
@@ -7,58 +7,60 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     // 단일 숏츠의 댓글 개수 조회
     long countByShortsId(Long shortsId);
 
     // 전체 숏츠의 댓글 개수 조회
     @Query("""
-  select c.shorts.id as shortsId, count(c.id) as cnt
-  from Comment c
-  where c.shorts.id in :shortsIds
-  group by c.shorts.id
-""")
+              select c.shorts.id as shortsId, count(c.id) as cnt
+              from Comment c
+              where c.shorts.id in :shortsIds
+              group by c.shorts.id
+            """)
     List<ShortsCommentCountProjection> countAllCommentsByShortsIds(@Param("shortsIds") List<Long> shortsIds);
 
     // 댓글 조회
     @Query("""
-        SELECT c
-        FROM Comment c
-        JOIN FETCH c.user u
-        WHERE c.shorts.id = :shortsId
-        AND c.parent is null
-        ORDER BY c.createdAt desc
-""")
+                    SELECT c
+                    FROM Comment c
+                    JOIN FETCH c.user u
+                    WHERE c.shorts.id = :shortsId
+                    AND c.parent is null
+                    ORDER BY c.createdAt desc
+            """)
     List<Comment> findCommentsWithUser(@Param("shortsId") Long shortsId);
 
     // 대댓글 개수 조회
     @Query("""
-        SELECT r.parent.id AS parentId, COUNT(r.id) AS cnt
-        FROM Comment r
-        WHERE r.parent.id IN :parentIds
-        GROUP BY r.parent.id
-    """)
+                SELECT r.parent.id AS parentId, COUNT(r.id) AS cnt
+                FROM Comment r
+                WHERE r.parent.id IN :parentIds
+                GROUP BY r.parent.id
+            """)
     List<ReplyCountProjection> countRepliesByParentIds(@Param("parentIds") List<Long> parentIds);
 
     // 대댓글 조회
     @Query("""
-    SELECT c
-    FROM Comment c
-    JOIN FETCH c.user u
-    WHERE c.parent.id = :parentId
-    ORDER BY c.createdAt ASC
-""")
+                SELECT c
+                FROM Comment c
+                JOIN FETCH c.user u
+                WHERE c.parent.id = :parentId
+                ORDER BY c.createdAt ASC
+            """)
     List<Comment> findRepliesWithUser(@Param("parentId") Long parentId);
 
     public interface ReplyCountProjection {
 
         Long getParentId();
+
         long getCnt();
     }
 
     public interface ShortsCommentCountProjection {
         Long getShortsId();
+
         long getCnt();
     }
 }
