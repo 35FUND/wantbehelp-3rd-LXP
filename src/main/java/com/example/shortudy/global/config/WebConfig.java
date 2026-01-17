11ad61376803045420a2ef9d868ee2a45e6f0b22
minji0828 <entersuk1998@naver.com>
2026-01-18 02:23:20 +0900

    Update WebConfig.java

--- src/main/java/com/example/shortudy/global/config/WebConfig.java
+++ src/main/java/com/example/shortudy/global/config/WebConfig.java
@@ -2,11 +2,15 @@ package com.example.shortudy.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -26,6 +30,13 @@ public class WebConfig implements WebMvcConfigurer {
         Path uploadPath = Paths.get(uploadDir).toAbsolutePath();
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath.toString() + "/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(converter -> converter instanceof StringHttpMessageConverter)
+                .forEach(converter -> ((StringHttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8));
     }
 }
 
