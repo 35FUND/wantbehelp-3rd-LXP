c71447061601fd591f0a7d400faa9b09bbf2bfd1
hun <dksxlahsl@naver.com>
2026-02-09 03:57:04 +0900

    refactor[recommendation]: 도메인 리팩토링 및 성능 최적화 (#109)

---
+++ src/main/java/com/example/shortudy/domain/playlist/playlist/dto/response/OwnerInfo.java
@@ -0,0 +1,21 @@
+package com.example.shortudy.domain.playlist.playlist.dto.response;
+
+import com.example.shortudy.domain.user.entity.User;
+
+/**
+ * 플레이리스트 소유자 정보 DTO
+ * - PlaylistResponse, PlaylistDetailResponse 등에서 공통으로 사용
+ */
+public record OwnerInfo(
+        Long id,            // 소유자 ID
+        String nickname,    // 닉네임
+        String profileUrl   // 프로필 이미지 URL
+) {
+    public static OwnerInfo from(User user) {
+        return new OwnerInfo(
+                user.getId(),
+                user.getNickname(),
+                user.getProfileUrl()
+        );
+    }
+}
